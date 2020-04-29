04832d5d401dd7d318ba016db5a6cce522bfe5ba
Jeremy ho <eneka.ken@gmail.com>
2020-04-28 17:02:37 -0700

    initial commit, file structure setup

---
+++ client/src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component{
+
+
+render(){
+  return(
+
+  )
+}
+
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
