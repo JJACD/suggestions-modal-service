8a099ac631583220f5b12277ef3e9b5c4895dae0
Jeremy ho <eneka.ken@gmail.com>
2020-05-01 15:44:15 -0700

    :completed seed file

--- seed.js
+++ seed.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://enekaken:<Moranma1/27921>@cluster0-x3i24.mongodb.net/test?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true});
 
-var db = mongoose.connection;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useUnifiedTopology: true, useNewUrlParser: true});
+
+//const Suggestionsdata = require('./database/index.js')
+
+const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
     // we're connected!
   });
-
-
+  //db.suggestionsdatas.drop()
 
 const suggestionsSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   productname: String,
   productprice: Number,
   productimgurl: String,
-  productid: Number,
+  productid: String,
   producturl: String,
   producttype: {
     collection: String, //ABC, commision, metal vent tech, city swat, surge
@@ -26,10 +28,16 @@ const suggestionsSchema = mongoose.Schema({
   bestseller: Boolean //yes or no
 });
 
-
-
 const Suggestionsdata = mongoose.model('Suggestionsdata', suggestionsSchema);
 
+const prices = [
+  '88',
+  '98',
+  '118',
+  '128',
+  '138',
+  '148'
+];
 
 const mensjogger = [
   'Textured Tech Jogger',
@@ -56,38 +64,34 @@ const mensjogger = [
   'Refract Jogger 29" lululemon lab'
   ];
 
-const prices = [
-  '88',
-  '98',
-  '118',
-  '128',
-  '138',
-  '148'
+const malejoggerpic = [
+  'LM5A04S_014033_1',
+  'LM5A39S_032798_1',
+  'LM5A60S_044354_1',
+  'LM5A71S_1966_1',
+  'LM5543S_034268_1',
+  'LM5556S_032476_1',
+  'LM5569S_1966_1',
+  'LM5574S_032476_1',
+  'LM5585T_0001_1',
+  'LM5603S_0001_1',
+  'LM5642S_038068_1',
+  'LM5654S_028849_1',
+  'LM5689S_032894_1',
+  'LM5709S_032476_1',
+  'LM5720T_026083_1',
+  'LM5730S_0001_1',
+  'LM5745S_0001_1',
+  'LM5918S_032476_1',
+  'LM5922T_030700_1',
+  'LM5942S_032798_1',
+  'LM5951S_031311_1'
 ];
-
-const malefilename = [
-  'LM5A04S_014033_1.webp',
-  'LM5A39S_032798_1.webp',
-  'LM5A60S_044354_1.webp',
-  'LM5A71S_1966_1.webp',
-  'LM5543S_034268_1.webp',
-  'LM5556S_032476_1.webp',
-  'LM5569S_1966_1.webp',
-  'LM5574S_032476_1.webp',
-  'LM5585T_0001_1.webp',
-  'LM5603S_0001_1.webp',
-  'LM5642S_038068_1.webp',
-  'LM5654S_028849_1.webp',
-  'LM5689S_032894_1.webp',
-  'LM5709S_032476_1.webp',
-  'LM5720T_026083_1.webp',
-  'LM5730S_0001_1.webp',
-  'LM5745S_0001_1.webp',
-  'LM5918S_032476_1.webp',
-  'LM5922T_030700_1.webp',
-  'LM5942S_032798_1.webp',
-  'LM5951S_031311_1.webp'
-];
+const mensjoggercollection = [
+  'ABC',
+  'City Sweat',
+  'Surge'
+]
 
 const mens_pants_subcategory = [
   'trousers',
@@ -96,11 +100,79 @@ const mens_pants_subcategory = [
   'tights',
 ];
 
-const gender = 'male'
-const subcategory = 'joggers'
-//const mensnurl = path.join('https://luluproject.s3-us-west-1.amazonaws.com', 'make', 'joggers', malefilename)
-// 'https://luluproject.s3-us-west-1.amazonaws.com/male/joggers/LM5543S_034268_1.webp'
-// const url = path.join('https://luluproject.s3-us-west-1.amazonaws.com', 'male', 'joggers', 'LM5543S_034268_1.webp')
+//-------------------------------------------------------------------------------------------------------
+const mensshirts = [
+  '5 Year Basic Tee',
+  '5 Year Basic Long Sleeve',
+  '5 Year Basic V',
+  'Metal Vent Tech Short Sleeve 2.0',
+  'Metal Vent Tech Long Sleeve 2.0',
+  'Metal Vent Breathe Short Sleeve',
+  'Evolution Polo',
+  'Down to the Wire Short Sleeve Shirt Online Only',
+  'Metal Vent Tech Short Sleeve V 2.0 Online Only',
+  '5 Year Basic Long Sleeve Henley',
+  'Fast and Free Short Sleeve',
+  'Fresh Form Short Sleeve',
+  '5 Year Basic Tee',
+  'Ashta Short Sleeve Tee lululemon lab',
+  'Metal Vent Tech Short Sleeve V 2.0 Online Only',
+  'Metal Vent Breathe Short Sleeve',
+  'Fast and Free Short Sleeve',
+  'Take The Moment Graphic Short Sleeve lululemon x Robert Geller',
+  '5 Year Basic Long Sleeve',
+  'Airing Easy Short Sleeve Buttondown',
+  'Confluence Short Sleeve Tee lululemon lab',
+  'Confluence Short Sleeve Crew lululemon lab',
+  'Grid Light Short Sleeve Shirt',
+  '5 Year Basic Long Sleeve Henley',
+  'Down to the Wire Slim Fit Short Sleeve Online Only'
+]
+const mensshirtspics = [
+  'LM3AP5S_013211_1',
+  'LM3AP5S_026914_1',
+  'LM3AP6S_0002_1',
+  'LM3ATBS_1966_1',
+  'LM3AV3S_044015_1',
+  'LM3ATBS_035943_1',
+  'LM3BHLS_035487_1',
+  'LM3BCQS_0002_1',
+  'LM3BETS_037032_1',
+  'LM3BKCS_038888_1',
+  'LM3BL4S_041982_1',
+  'LM3BLES_0572_1',
+  'LM3BLES_044318_1',
+  'LM3BPQS_040694_1',
+  'LM3BR5S_044315_1',
+  'LM3BTOS_034812_1',
+  'LM3BV1S_043757_1',
+  'LM3BVQS_4780_1',
+  'LM3BV7S_044039_1',
+  'LM3BY8S_044528_1',
+  'LM3CB3S_044611_1',
+  'LM3CB5S_044223_1',
+  'LM3CB8S_030210_1',
+  'LM3CC6S_040239_1',
+  'LM3CJLS_027976_1'
+]
+
+const mensshirtscollections = [
+  'Commission',
+  'Metal Vent Tech',
+  'Surge',
+  '5 Year Basics',
+  'Fast and Free'
+]
+
+const mens_shirts_subcategory = [
+  'Button Down Shirs',
+  'Polo Shirts',
+  'Short Sleeve Shirts',
+  'Long Sleeve Shirts',
+  'Tank Tops',
+  'T-Shirts'
+]
+//-------------------------------------------------------------------------------------------------------
 const womens_pants_subcategory = [
   'leggings',
   'capris',
@@ -134,74 +206,97 @@ const womenspants = [
   'Wunder Train High-Rise Tight 25" Diamond Dye',
   'In Movement Tight 25" Everlux',
 ];
-const womensfilename = [
-  'LW5BPJS_0001_1.webp',
-  'LW5BSZS_042629_1.webp',
-  'LW5BT2S_034135_1.webp',
-  'LW5BTES_042628_1.webp',
-  'LW5BXOS_026950_1.webp',
-  'LW5BXQS_034115_1.webp',
-  'LW5BY3S_042629_1.webp',
-  'LW5CGPS_043753_1.webp',
-  'LW5CIFS_034135_1.webp',
-  'LW5COUS_043990_1.webp',
-  'LW5CQAS_034135_1.webp',
-  'LW5CQDS_043619_1.webp',
-  'LW5CQMS_019223_1.webp',
-  'LW5CQSS_045102_1.webp',
-  'LW5CQVS_043990_1.webp',
-  'LW5CR9S_043989_1.webp',
-  'LW5DADS_047310_1.webp',
-  'LW6AVIS_034135_1.webp',
-  'LW6BDES_043991_1.webp',
-  'LW6BFBS_042628_1.webp',
+const womenspantspic = [
+  'LW5BPJS_0001_1',
+  'LW5BSZS_042629_1',
+  'LW5BT2S_034135_1',
+  'LW5BTES_042628_1',
+  'LW5BXOS_026950_1',
+  'LW5BXQS_034115_1',
+  'LW5BY3S_042629_1',
+  'LW5CGPS_043753_1',
+  'LW5CIFS_034135_1',
+  'LW5COUS_043990_1',
+  'LW5CQAS_034135_1',
+  'LW5CQDS_043619_1',
+  'LW5CQMS_019223_1',
+  'LW5CQSS_045102_1',
+  'LW5CQVS_043990_1',
+  'LW5CR9S_043989_1',
+  'LW5DADS_047310_1',
+  'LW6AVIS_034135_1',
+  'LW6BDES_043991_1',
+  'LW6BFBS_042628_1',
 ];
-//const womenurl = path.join('https://luluproject.s3-us-west-1.amazonaws.com', gender, womens_pants_subcategory, womensfilename);
+const womenspantscollection = [
+  'align',
+  'Wunder Under',
+  'Fast & Free',
+  'On The Fly',
+  'In Movement',
+]
 
-/*
-const womensitem = new Suggestionsdata({
-  _id: new mongoose.Schema.Types.ObjectId,
-  productname: womenspants,
-  productprice: prices,
-  productimgurl: womenurl,
-  productid: womensfilename,
-  producttype: {
-    subcategory: womens_pants_subcategory
-  },
-});
 
-const mensitem = new Suggestionsdata({
-  _id: new mongoose.Schema.Types.ObjectId,
-  productname: mensjogger,
-  productprice: prices,
-  productimgurl: mensnurl,
-  productid: malefilename,
-  producttype: {
-    subcategory: mens_pants_subcategory
-  },
-});
-*/
 
-//mensJoggerObj = mensjogger.map(name => {
-  // const malobj = new Suggestionsdata({
-  //   _id: new mongoose.Types.ObjectId,
-  //   productname: mensjogger[0],
-  //   productprice: prices[Math.floor(Math.random() * prices.length)],
-  //   productid: malefilename[Math.floor(Math.random() * malefilename.length)],
-  //   productimgurl: `https://luluproject.s3-us-west-1.amazonaws.com/male/joggers/${malefilename[Math.floor(Math.random() * malefilename.length)]}`,
-  //   producttype: {
-  //     subcategory: mens_pants_subcategory[Math.floor(Math.random() * mens_pants_subcategory.length)]
-  //   },
-  // });
-  //})
-
-  const malobj = new Suggestionsdata({
+//-----------------------------------------Generates Mens Jogger Data--------------------------------
+mensjogger.map(name => {
+  name = new Suggestionsdata({
     _id: new mongoose.Types.ObjectId,
-    productname: 'test product name',
-    productprice: '123',
-    productid: 'abc123',
-    productimgurl: 'https://luluproject.s3-us-west-1.amazonaws.com/male/joggers//LW5DADS_047310_1.webp',
+    productname: mensjogger[Math.floor(Math.random() * mensjogger.length)],
+    productprice: prices[Math.floor(Math.random() * prices.length)],
+    productid: malejoggerpic[Math.floor(Math.random() * malejoggerpic.length)],
+    productimgurl: `https://luluproject.s3-us-west-1.amazonaws.com/male/joggers/${malejoggerpic[Math.floor(Math.random() * malejoggerpic.length)]}.webp`,
     producttype: {
-      subcategory: 'joggers'
+      collection: mensjoggercollection[Math.floor(Math.random() * mensjoggercollection.length)],
+      gender: 'mens',
+      subcategory: 'joggers',
+      category: 'clothes'
     },
   });
+  name.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    console.log('saved', name);
+  });
+  })
+
+//-----------------------------------------Generates Womens Jogger Data--------------------------------
+womenspants.map(name => {
+  name = new Suggestionsdata({
+    _id: new mongoose.Types.ObjectId,
+    productname: womenspants[Math.floor(Math.random() * womenspants.length)],
+    productprice: prices[Math.floor(Math.random() * prices.length)],
+    productid: womenspantspic[Math.floor(Math.random() * womenspantspic.length)],
+    productimgurl: `https://luluproject.s3-us-west-1.amazonaws.com/female/pants/${womenspantspic[Math.floor(Math.random() * womenspantspic.length)]}.webp`,
+    producttype: {
+      collection: womenspantscollection[Math.floor(Math.random() * womenspantscollection.length)],
+      gender: 'womens',
+      subcategory:womens_pants_subcategory[Math.floor(Math.random() * womens_pants_subcategory.length)],
+      category: 'clothes'
+    },
+  });
+  name.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    console.log('saved', name);
+  });
+  })
+//-----------------------------------------Generates Mens Shirts Data--------------------------------
+mensshirts.map(name => {
+  name = new Suggestionsdata({
+    _id: new mongoose.Types.ObjectId,
+    productname: mensshirts[Math.floor(Math.random() * mensshirts.length)],
+    productprice: prices[Math.floor(Math.random() * prices.length)],
+    productid: mensshirtspics[Math.floor(Math.random() * mensshirtspics.length)],
+    productimgurl: `https://luluproject.s3-us-west-1.amazonaws.com/male/shirts/${mensshirtspics[Math.floor(Math.random() * mensshirtspics.length)]}.webp`,
+    producttype: {
+      collection: mensshirtscollections[Math.floor(Math.random() * mensshirtscollections.length)],
+      gender: 'mens',
+      subcategory:mens_shirts_subcategory[Math.floor(Math.random() * mens_shirts_subcategory.length)],
+      category: 'clothes'
+    },
+  });
+  name.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    console.log('saved', name);
+  });
+  })
+
